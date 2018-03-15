6c28c56e58cde51d007a88a6aef7f2da87bf44da
arowley2001 <37371814+arowley2001@users.noreply.github.com>
2018-03-15 08:34:27 -0400

    Updated from Brython Server: 3/15/2018 8:34:25 AM

--- rocket2.py
+++ rocket2.py
@@ -1,16 +1,56 @@
 from ggrocket import Rocket, Planet
-from math import radians, sqrt
+from math import radians, sqrt, log
+from ggmath import InputButton, Timer
 
-earth = Planet()
+earth = Planet(planetmass=0)  # no gravity to simplify things
 
-# Constants relating to Earth and physics
-Re = 6.371E6  # Earth radius: 6371000 meters in scientific notation
-Me = 5.972E24 # Earth mass in kg (5.972 x 10^24)
-G = 6.674E-11 # Gravitational constant
+RocketStarted = False
+StartTime = None    # to keep track of when burn started
+BurnTime = 0        # keep track of how much time the burn has lasted
 
-# Calculate the escape velocity from Earth's surface radius
-Ve=sqrt(2*Me*G/Re)
-print("Predicted escape velocity is ", Ve, " m/s")
+# Falcon F9R specifications
+me = 25600          # Empty mass
+mp =  395700        # Propellent mass
+F1D = 716000        # Single engine thrust (Newtons)
+N1D = 9             # Number of rocket engines
+Ftotal = F1D * N1D  # Total thrust (Newtons)
+tburn = 180         # Burn time (seconds)
 
-rocket = Rocket(earth, heading=radians(90), directiond=90, velocity=Ve)
+# Predict the final velocity based on simple Newtons' 2nd Law
+vmax = Ftotal*tburn/(me+mp)
+print("Predicted final velocity (Newton's 2nd Law), vmax: ", vmax, " m/s")
+
+# Create a function for determining the rocket thrust
+def GetThrust():
+    global BurnTime
+    global RocketStarted
+    if RocketStarted:
+        # get the burn time: seconds since start
+        BurnTime = rocket.shiptime - StartTime
+        # is it time to stop the rocket?
+        if BurnTime >= tburn:
+            # stop the rocket and report zero thrust
+            RocketStarted = False
+            return 0
+        else:
+            # still burning, report full thrust
+            return Ftotal
+    else:
+        return 0
+
+# Function for starting the rocket thrust (called by the START "button")
+def StartRocket():
+    global RocketStarted
+    global StartTime
+    if not RocketStarted:
+        RocketStarted = True
+        # Note the starting time
+        StartTime = rocket.shiptime
+        
+# Create a button for starting the simulation
+# Physical positioning at 10,400 pixels, calls the StartRocket function
+start = InputButton((10,400), "START", StartRocket, positioning="physical", size=15)
+
+#Create and "run" the rocket
+rocket = Rocket(earth, thrust=GetThrust, mass=mp+me)
 earth.run(rocket)
